07f71e9bc800477d485a52c682566b7773900e12
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-23 14:21:18 -0600

    updt: app controller

--- src/main/java/com/eduardo/springboothorario/controllers/AppController.java
+++ src/main/java/com/eduardo/springboothorario/controllers/AppController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Data
 public class AppController {
     @Value("${config.horario.apertura}")
     private Integer apertura;
@@ -19,5 +18,19 @@ public class AppController {
     public String index(Model model){
         model.addAttribute("titulo", "Bienvenido al horario de atencion al cliente");
         return "index";
+    }
+
+    @GetMapping("/cerrado")
+    public String cerrado(Model model){
+        StringBuilder mensaje = new StringBuilder("Cerrado, por favor visitanos desde las ");
+        mensaje.append(apertura);
+        mensaje.append(" y las ");
+        mensaje.append(cierre);
+        mensaje.append(" hrs. Gracias.");
+
+        model.addAttribute("titulo", "Fuera del horario de atencion");
+        model.addAttribute("mensaje", mensaje);
+
+        return "cerrado";
     }
 }
